8a756190a9fc586b126948a73a24a4b38fb35ce0
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-26 00:21:58 +0300

    feat: expose public geo catalog API

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export { GEO_ENTITIES, getGeoEntity, hasGeoEntity, findGeoEntities, getGeoChildren } from './catalog.js';
+export { isValidCoordinate, containsPoint, distanceKm, nearestGeoEntity } from './spatial.js';
+export { validateGeoCatalog } from './validate.js';
