f033f64f0ceb98d51fe9269a3dfac4ec6b73b51c
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-26 10:47:50 +0300

    data: wire Uzbekistan regional anchors

--- src/data/uz-tashkent-region-city-anchors.js
+++ src/data/uz-tashkent-region-city-anchors.js
@@ -1,3 +1,5 @@
+import { UZ_REGIONAL_ANCHORS } from './uz-regional-anchors.js';
+
 const osmPoi = (citySlug, slug, canonicalName, lat, lng, osmType, osmId, accuracyM = 130) => ({
   id: `uz:${citySlug}:poi:${slug}`,
   type: 'poi',
@@ -14,4 +16,5 @@ const osmPoi = (citySlug, slug, canonicalName, lat, lng, osmType, osmId, accurac
 export const UZ_TASHKENT_REGION_CITY_ANCHORS = Object.freeze([
   osmPoi('angren', 'angren-railway-station', 'Angren Railway Station', 40.99905, 70.08266, 'node', 1412998292, 110),
   osmPoi('almalyk', 'olmaliq-bus-station', 'Olmaliq Bus Station', 40.86406, 69.59269, 'way', 257730061, 140),
+  ...UZ_REGIONAL_ANCHORS,
 ]);
