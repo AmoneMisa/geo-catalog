719496d65252a533be862f204216661534524dc8
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-27 01:20:57 +0300

    data: add Kyiv central landmark anchors

--- src/data/ua/kyiv/poi.js
+++ src/data/ua/kyiv/poi.js
@@ -1,50 +1,32 @@
-const sourcedPark = (slug, canonicalName, lat, lng, sourceUrl, accuracyM = 350) => Object.freeze({
+const poi = (slug, type, canonicalName, lat, lng, accuracyM, extra = {}) => Object.freeze({
   id: `ua:kyiv:poi:${slug}`,
-  type: 'poi.park',
+  type,
   country: 'UA',
   canonicalName,
   parentId: 'ua:kyiv',
   center: Object.freeze({ lat, lng }),
-  source: 'manual',
+  source: extra.osm ? 'osm' : extra.wikidataId ? 'wikidata' : 'manual',
   accuracy: 'poi',
   accuracyM,
-  sourceUrl,
-});
-
-const wikidataPark = (slug, canonicalName, lat, lng, wikidataId, accuracyM = 350) => Object.freeze({
-  id: `ua:kyiv:poi:${slug}`,
-  type: 'poi.park',
-  country: 'UA',
-  canonicalName,
-  parentId: 'ua:kyiv',
-  center: Object.freeze({ lat, lng }),
-  source: 'wikidata',
-  accuracy: 'poi',
-  accuracyM,
-  wikidataId,
-});
-
-const osmPark = (slug, canonicalName, lat, lng, osmType, osmId, wikidataId, accuracyM = 350) => Object.freeze({
-  id: `ua:kyiv:poi:${slug}`,
-  type: 'poi.park',
-  country: 'UA',
-  canonicalName,
-  parentId: 'ua:kyiv',
-  center: Object.freeze({ lat, lng }),
-  source: 'osm',
-  accuracy: 'poi',
-  accuracyM,
-  osm: Object.freeze({ type: osmType, id: osmId }),
-  ...(wikidataId ? { wikidataId } : {}),
+  ...extra,
 });
 
 export const UA_KYIV_POI_ENTITIES = Object.freeze([
-  wikidataPark('shevchenko-park', 'Парк Тараса Шевченка', 50.441667, 30.513056, 'Q12138938', 320),
-  sourcedPark('mariinskyi-park', 'Маріїнський парк', 50.44694, 30.53972, 'https://commons.wikimedia.org/wiki/Category:Mariinskyi_Park_(Kyiv)', 420),
-  osmPark('eternal-glory-park', 'Парк Вічної Слави', 50.43832, 30.55737, 'way', 41079798, 'Q4345217', 420),
-  wikidataPark('holosiivskyi-park', 'Голосіївський парк', 50.390278, 30.507222, 'Q3399767', 650),
-  osmPark('natalka-park', 'Парк Наталка', 50.49539, 30.52539, 'way', 23858027, 'Q93600485', 420),
-  wikidataPark('peremoha-park', 'Парк Перемога', 50.464719, 30.605831, 'Q3400191', 520),
-  wikidataPark('kyoto-park', 'Парк Кіото', 50.463639, 30.636331, 'Q12138898', 420),
-  osmPark('syretskyi-park', 'Сирецький парк', 50.46744, 30.43878, 'way', 113311396, 'Q28745492', 420),
+  poi('taras-shevchenko-park', 'poi.park', 'Парк Тараса Шевченка', 50.44133, 30.51143, 350, { wikidataId: 'Q4441787' }),
+  poi('mariinsky-park', 'poi.park', 'Маріїнський парк', 50.44755, 30.54184, 500, { wikidataId: 'Q2737272' }),
+  poi('eternal-glory-park', 'poi.park', 'Парк Вічної Слави', 50.43895, 30.55356, 450, { wikidataId: 'Q4343817' }),
+  poi('holosiivskyi-park', 'poi.park', 'Голосіївський парк', 50.38744, 30.50179, 900, { wikidataId: 'Q4137844' }),
+  poi('natalka-park', 'poi.park', 'Парк Наталка', 50.50463, 30.51635, 700, { wikidataId: 'Q4344851' }),
+  poi('peremoha-park', 'poi.park', 'Парк Перемога', 50.46345, 30.60532, 850, { wikidataId: 'Q4344575' }),
+  poi('kyoto-park', 'poi.park', 'Парк Кіото', 50.46445, 30.64479, 800, { wikidataId: 'Q4344289' }),
+  poi('syretskyi-park', 'poi.park', 'Сирецький парк', 50.47368, 30.44074, 700, { wikidataId: 'Q4447980' }),
+
+  poi('maidan-nezalezhnosti', 'poi.square', 'Майдан Незалежності', 50.45025, 30.523889, 220, { wikidataId: 'Q863759' }),
+  poi('sofiiska-square', 'poi.square', 'Софійська площа', 50.453611, 30.516111, 180, { wikidataId: 'Q3406857', osm: Object.freeze({ type: 'relation', id: 9298900 }) }),
+  poi('mykhailivska-square', 'poi.square', 'Михайлівська площа', 50.455556, 30.521111, 180),
+  poi('kontraktova-square', 'poi.square', 'Контрактова площа', 50.463889, 30.518056, 220, { wikidataId: 'Q2429499', osm: Object.freeze({ type: 'relation', id: 354367 }) }),
+  poi('poshtova-square', 'poi.square', 'Поштова площа', 50.459439, 30.525, 180, { wikidataId: 'Q2472423' }),
+  poi('andriivskyi-descent', 'poi.street', 'Андріївський узвіз', 50.459861, 30.515139, 500, { wikidataId: 'Q2622438' }),
+  poi('golden-gate', 'poi.landmark', 'Золоті ворота', 50.448333, 30.513056, 120, { wikidataId: 'Q1427503' }),
+  poi('kyiv-pechersk-lavra', 'poi.landmark', 'Києво-Печерська лавра', 50.432222, 30.562222, 650, { wikidataId: 'Q242711' }),
 ]);
