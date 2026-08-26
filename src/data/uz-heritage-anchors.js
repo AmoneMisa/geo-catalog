33e21cca4df393112c90c96f42b5f60b314d1a18
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-26 10:51:00 +0300

    data: align Kokand palace canonical

--- src/data/uz-heritage-anchors.js
+++ src/data/uz-heritage-anchors.js
@@ -15,5 +15,5 @@ const wikidataPoi = (citySlug, slug, canonicalName, lat, lng, wikidataId, extra 
 export const UZ_HERITAGE_ANCHORS = Object.freeze([
   wikidataPoi('khiva', 'itchan-kala', 'Itchan Kala', 41.377718, 60.359476, 'Q535577', { accuracyM: 350 }),
   wikidataPoi('shakhrisabz', 'ak-saray-palace', 'Ak-Saray Palace', 39.060776, 66.829475, 'Q2828887', { accuracyM: 140 }),
-  wikidataPoi('kokand', 'khudayar-khan-palace', 'Palace of Khudayar Khan', 40.538333, 70.937500, 'Q7126242', { osm: { type: 'way', id: 174718684 }, accuracyM: 100 }),
+  wikidataPoi('kokand', 'khudayar-khan-palace', 'Khudoyar Khan Palace', 40.538333, 70.937500, 'Q7126242', { osm: { type: 'way', id: 174718684 }, accuracyM: 100 }),
 ]);
