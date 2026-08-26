846fc4ade602e5a73ebcc7b33e79f8019a365cae
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-26 06:56:03 +0300

    data: include Chilanzar microdistricts 15 through 20

--- src/catalog.js
+++ src/catalog.js
@@ -5,6 +5,7 @@ import { TASHKENT_METRO_EXTRA_ENTITIES } from './data/tashkent-metro-extra.js';
 import { TASHKENT_MICRODISTRICT_ENTITIES } from './data/tashkent-microdistricts.js';
 import { TASHKENT_MICRODISTRICT_EXTRA_ENTITIES } from './data/tashkent-microdistricts-extra.js';
 import { TASHKENT_CHILANZAR_11_14_ENTITIES } from './data/tashkent-microdistricts-chilanzar-11-14.js';
+import { TASHKENT_CHILANZAR_15_20_ENTITIES } from './data/tashkent-microdistricts-chilanzar-15-20.js';
 import { TASHKENT_POI_ENTITIES } from './data/tashkent-poi.js';
 import { TASHKENT_POI_EXTRA_ENTITIES } from './data/tashkent-poi-extra.js';
 import { TASHKENT_RESIDENTIAL_ENTITIES } from './data/tashkent-residential.js';
@@ -18,6 +19,7 @@ const entities = [
   ...TASHKENT_MICRODISTRICT_ENTITIES,
   ...TASHKENT_MICRODISTRICT_EXTRA_ENTITIES,
   ...TASHKENT_CHILANZAR_11_14_ENTITIES,
+  ...TASHKENT_CHILANZAR_15_20_ENTITIES,
   ...TASHKENT_POI_ENTITIES,
   ...TASHKENT_POI_EXTRA_ENTITIES,
   ...TASHKENT_RESIDENTIAL_ENTITIES,
