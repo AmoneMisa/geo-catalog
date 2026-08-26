d588cd9c23b9c330ed3614fa3fa2b26e8a87f89b
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-26 07:42:48 +0300

    data: add Samarkand park and family POIs

--- src/data/samarkand-poi.js
+++ src/data/samarkand-poi.js
@@ -26,6 +26,18 @@ const osmPoi = (slug, canonicalName, lat, lng, osmType, osmId, accuracyM = 140, 
   ...(wikidataId ? { wikidataId } : {}),
 });
 
+const manualPoi = (slug, canonicalName, lat, lng, accuracy = 'building', accuracyM = 160) => ({
+  id: `uz:samarkand:poi:${slug}`,
+  type: 'poi',
+  country: 'UZ',
+  canonicalName,
+  parentId: 'uz:samarkand',
+  center: { lat, lng },
+  source: 'manual',
+  accuracy,
+  accuracyM,
+});
+
 export const SAMARKAND_POI_ENTITIES = Object.freeze([
   wikidataPoi('registan-square', 'Registan Square', 39.654722, 66.975556, 'Q1373583', 120, { type: 'relation', id: 17141748 }),
   wikidataPoi('gur-e-amir', 'Gur-e Amir', 39.648333, 66.968889, 'Q1256223', 100),
@@ -38,4 +50,6 @@ export const SAMARKAND_POI_ENTITIES = Object.freeze([
   osmPoi('silk-road-samarkand', 'Silk Road Samarkand', 39.65960, 67.05576, 'node', 12536653209, 150),
   osmPoi('afrosiyob', 'Afrosiyob', 39.67045, 66.98799, 'way', 110296439, 250, 'Q2167520'),
   osmPoi('eternal-city', 'Eternal City', 39.65384, 67.06127, 'way', 1083701137, 180),
+  osmPoi('alisher-navoiy-park', 'Alisher Navoiy Park', 39.64968, 66.95803, 'way', 203548053, 220),
+  manualPoi('family-park', 'Family Park', 39.662847, 66.907978, 'building', 180),
 ]);
