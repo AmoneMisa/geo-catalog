60e8fd0866b195843027573e5de6fc129957981c
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-26 10:54:55 +0300

    data: add verified Khiva heritage anchors

--- src/data/uz-heritage-anchors.js
+++ src/data/uz-heritage-anchors.js
@@ -13,7 +13,9 @@ const wikidataPoi = (citySlug, slug, canonicalName, lat, lng, wikidataId, extra 
 });
 
 export const UZ_HERITAGE_ANCHORS = Object.freeze([
-  wikidataPoi('khiva', 'itchan-kala', 'Itchan Kala', 41.377718, 60.359476, 'Q535577', { accuracyM: 350 }),
+  wikidataPoi('khiva', 'itchan-kala', 'Ichan Kala', 41.37810, 60.35980, 'Q535577', { osm: { type: 'way', id: 185036984 }, accuracyM: 250 }),
+  wikidataPoi('khiva', 'kalta-minor', 'Kalta Minor', 41.37835, 60.35798, 'Q4294004', { osm: { type: 'way', id: 178606424 }, accuracyM: 90 }),
+  wikidataPoi('khiva', 'kunya-ark', 'Kunya Ark', 41.37888, 60.35822, 'Q4247358', { osm: { type: 'node', id: 1956482954 }, accuracyM: 100 }),
   wikidataPoi('shakhrisabz', 'ak-saray-palace', 'Ak-Saray Palace', 39.060776, 66.829475, 'Q2828887', { accuracyM: 140 }),
   wikidataPoi('kokand', 'khudayar-khan-palace', 'Khudoyar Khan Palace', 40.538333, 70.937500, 'Q7126242', { osm: { type: 'way', id: 174718684 }, accuracyM: 100 }),
 ]);
