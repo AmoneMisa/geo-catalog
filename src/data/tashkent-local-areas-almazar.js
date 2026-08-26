0b57f1b3d45fa7c57433c665e7b083c707ef69f4
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-26 07:01:04 +0300

    data: add Almazar local areas

---
+++ src/data/tashkent-local-areas-almazar.js
@@ -0,0 +1,19 @@
+const osmLocalArea = (slug, canonicalName, lat, lng, osmType, osmId, accuracyM = 600) => ({
+  id: `uz:tashkent:local-area:${slug}`,
+  type: 'local_area',
+  country: 'UZ',
+  canonicalName,
+  parentId: 'uz:tashkent:almazar',
+  center: { lat, lng },
+  source: 'osm',
+  accuracy: 'neighborhood',
+  accuracyM,
+  osm: { type: osmType, id: osmId },
+});
+
+export const TASHKENT_ALMAZAR_LOCAL_AREA_ENTITIES = Object.freeze([
+  osmLocalArea('karakamysh-1-2', 'Karakamysh-1/2', 41.35729, 69.23594, 'node', 1866791241),
+  osmLocalArea('karakamysh-2-3', 'Karakamysh-2/3', 41.36769, 69.21694, 'way', 134756857),
+  osmLocalArea('karakamysh-2-4', 'Karakamysh-2/4', 41.36786, 69.20858, 'node', 1866058432),
+  osmLocalArea('karakamysh-2-5', 'Karakamysh-2/5', 41.36092, 69.20909, 'node', 1866058434),
+]);
