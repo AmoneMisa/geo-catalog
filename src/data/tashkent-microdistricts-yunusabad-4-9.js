7b1206cddf0c7858809deb4f9fec1d1f1520afc2
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-26 06:56:20 +0300

    data: add verified Yunusabad microdistricts 4 through 9

---
+++ src/data/tashkent-microdistricts-yunusabad-4-9.js
@@ -0,0 +1,20 @@
+const osmMicrodistrict = (slug, canonicalName, lat, lng, osmType, osmId, accuracyM = 550) => ({
+  id: `uz:tashkent:microdistrict:${slug}`,
+  type: 'microdistrict',
+  country: 'UZ',
+  canonicalName,
+  parentId: 'uz:tashkent',
+  center: { lat, lng },
+  source: 'osm',
+  accuracy: 'neighborhood',
+  accuracyM,
+  osm: { type: osmType, id: osmId },
+});
+
+export const TASHKENT_YUNUSABAD_4_9_ENTITIES = Object.freeze([
+  osmMicrodistrict('yunusabad-4', 'Yunusabad-4', 41.3657, 69.28216, 'node', 2717111033),
+  osmMicrodistrict('yunusabad-6', 'Yunusabad-6', 41.36884, 69.26419, 'node', 1867002805),
+  osmMicrodistrict('yunusabad-7', 'Yunusabad-7', 41.37128, 69.26983, 'way', 917106773),
+  osmMicrodistrict('yunusabad-8', 'Yunusabad-8', 41.37129, 69.28102, 'node', 4467322432),
+  osmMicrodistrict('yunusabad-9', 'Yunusabad-9', 41.37658, 69.27707, 'node', 1866983404),
+]);
