70bf471563bd2067f730f09dd12965515ad14829
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-26 20:46:46 +0300

    data: add Alisher Navoiy Park anchor

--- src/data/uz-secondary-poi-anchors.js
+++ src/data/uz-secondary-poi-anchors.js
@@ -25,11 +25,25 @@ const wikidataPoi = (citySlug, slug, canonicalName, lat, lng, wikidataId, accura
   wikidataId,
 });
 
+const sourcedPoi = (citySlug, slug, canonicalName, lat, lng, sourceUrl, accuracyM = 220) => ({
+  id: `uz:${citySlug}:poi:${slug}`,
+  type: 'poi',
+  country: 'UZ',
+  canonicalName,
+  parentId: `uz:${citySlug}`,
+  center: { lat, lng },
+  source: 'manual',
+  accuracy: 'poi',
+  accuracyM,
+  sourceUrl,
+});
+
 export const UZ_SECONDARY_POI_ANCHORS = Object.freeze([
   osmPoi('margilan', 'kumtepa-bazaar', 'Kumtepa Bazaar', 40.45504, 71.66594, 'way', 253749024, 180),
   osmPoi('almalyk', 'metallurg-stadium', 'Metallurg Stadium', 40.84495, 69.60070, 'way', 257413698, 170, 'Q5927465'),
   osmPoi('kokand', 'kokand-bazaar', 'Kokand Bazaar', 40.55218, 70.95907, 'way', 174506939, 190),
   wikidataPoi('navoiy', 'farhod-palace-of-culture', 'Farhod Palace of Culture', 40.094, 65.38, 'Q100813546', 140),
+  sourcedPoi('navoiy', 'alisher-navoiy-park', 'Alisher Navoiy Park', 40.1083076, 65.3695758, 'https://yandex.com/maps/org/alisher_navoiy_national_park/95222732357/', 220),
   wikidataPoi('kosonsoy', 'mug-qala', 'Mug qala', 41.27608, 71.540155, 'Q122672687', 180),
   {
     id: 'uz:navoiy:poi:nmmc',
