9a3ddc124dadca372544799dba1c95f177870870
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-26 11:33:56 +0300

    data: add Urgut Bazaar and UzAuto Motors anchors

--- src/data/uz-tail-poi-anchors.js
+++ src/data/uz-tail-poi-anchors.js
@@ -15,4 +15,6 @@ export const UZ_TAIL_POI_ANCHORS = Object.freeze([
   osmPoi('chust', 'chust-bazaar', 'Chust Bazaar', 40.99732, 71.22677, 'way', 399882284, 120),
   osmPoi('beruniy', 'beruniy-bazaar', 'Beruniy Bazaar', 41.69063, 60.73921, 'way', 1177242145, 120),
   osmPoi('xonobod', 'xonobod-sanatorium', 'Xonobod Sanatorium', 40.80774, 72.98207, 'way', 600950022, 180),
+  osmPoi('urgut', 'urgut-bazaar', 'Urgut Bazaar', 39.42980, 67.18403, 'way', 252886374, 160),
+  osmPoi('asaka', 'uzauto-motors', 'UzAuto Motors', 40.65743, 72.23151, 'way', 27013443, 220),
 ]);
