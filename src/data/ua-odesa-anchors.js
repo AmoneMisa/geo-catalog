437739aa77e898796671025bcd1b286c215532c5
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-26 15:01:17 +0300

    Move Odesa suburb coordinate into geo catalog

---
+++ src/data/ua-odesa-anchors.js
@@ -0,0 +1,13 @@
+export const UA_ODESA_ANCHORS = Object.freeze([
+  Object.freeze({
+    id: 'ua:odesa:suburb:kryzhanivka',
+    type: 'suburb',
+    country: 'UA',
+    canonicalName: 'Крижанівка',
+    parentId: 'ua:odesa',
+    center: Object.freeze({ lat: 46.5617, lng: 30.7961 }),
+    source: 'manual',
+    accuracy: 'neighborhood',
+    accuracyM: 1200,
+  }),
+]);
