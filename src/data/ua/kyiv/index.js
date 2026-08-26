ca0edebf35f307d69f74fe087fd36247502278a4
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-27 00:00:47 +0300

    data: add verified Kyiv neighborhood anchors

--- src/data/ua/kyiv/index.js
+++ src/data/ua/kyiv/index.js
@@ -1,7 +1,9 @@
 import { UA_KYIV_ANCHORS } from '../../ua-kyiv-anchors.js';
 import { UA_KYIV_EXTRA_ANCHORS } from '../../ua-kyiv-anchors-extra.js';
+import { UA_KYIV_NEIGHBORHOODS } from './neighborhoods.js';
 
 export const UA_KYIV_ENTITIES = Object.freeze([
   ...UA_KYIV_ANCHORS,
   ...UA_KYIV_EXTRA_ANCHORS,
+  ...UA_KYIV_NEIGHBORHOODS,
 ]);
---
+++ src/data/ua/kyiv/neighborhoods.js
@@ -0,0 +1,68 @@
+export const UA_KYIV_NEIGHBORHOODS = Object.freeze([
+  Object.freeze({
+    id: 'ua:kyiv:microdistrict:zvirynets',
+    type: 'microdistrict', country: 'UA', canonicalName: 'Звіринець', parentId: 'ua:kyiv',
+    center: Object.freeze({ lat: 50.41922, lng: 30.55325 }), source: 'osm', accuracy: 'neighborhood', accuracyM: 1300,
+    osm: Object.freeze({ type: 'node', id: 1308227685 }), wikidataId: 'Q4189587',
+  }),
+  Object.freeze({
+    id: 'ua:kyiv:microdistrict:chorna-hora',
+    type: 'microdistrict', country: 'UA', canonicalName: 'Чорна гора', parentId: 'ua:kyiv',
+    center: Object.freeze({ lat: 50.41035, lng: 30.53995 }), source: 'osm', accuracy: 'neighborhood', accuracyM: 900,
+    osm: Object.freeze({ type: 'node', id: 1308227676 }), wikidataId: 'Q4518648',
+  }),
+  Object.freeze({
+    id: 'ua:kyiv:microdistrict:busove-pole',
+    type: 'microdistrict', country: 'UA', canonicalName: 'Бусове поле', parentId: 'ua:kyiv',
+    center: Object.freeze({ lat: 50.40677, lng: 30.546 }), source: 'osm', accuracy: 'neighborhood', accuracyM: 900,
+    osm: Object.freeze({ type: 'node', id: 6880330898 }), wikidataId: 'Q4100103',
+  }),
+  Object.freeze({
+    id: 'ua:kyiv:microdistrict:vidradnyi',
+    type: 'microdistrict', country: 'UA', canonicalName: 'Відрадний', parentId: 'ua:kyiv',
+    center: Object.freeze({ lat: 50.437, lng: 30.4149 }), source: 'osm', accuracy: 'neighborhood', accuracyM: 1700,
+    osm: Object.freeze({ type: 'node', id: 2962022995 }), wikidataId: 'Q4339989',
+  }),
+  Object.freeze({
+    id: 'ua:kyiv:microdistrict:mysholovka',
+    type: 'microdistrict', country: 'UA', canonicalName: 'Мишоловка', parentId: 'ua:kyiv',
+    center: Object.freeze({ lat: 50.37323, lng: 30.52854 }), source: 'osm', accuracy: 'neighborhood', accuracyM: 1400,
+    osm: Object.freeze({ type: 'node', id: 1308283982 }), wikidataId: 'Q4309665',
+  }),
+  Object.freeze({
+    id: 'ua:kyiv:microdistrict:vydubychi',
+    type: 'microdistrict', country: 'UA', canonicalName: 'Видубичі', parentId: 'ua:kyiv',
+    center: Object.freeze({ lat: 50.41475, lng: 30.56799 }), source: 'osm', accuracy: 'neighborhood', accuracyM: 1200,
+    osm: Object.freeze({ type: 'node', id: 2962022993 }), wikidataId: 'Q4128893',
+  }),
+  Object.freeze({
+    id: 'ua:kyiv:microdistrict:borshchahivka',
+    type: 'microdistrict', country: 'UA', canonicalName: 'Борщагівка', parentId: 'ua:kyiv',
+    center: Object.freeze({ lat: 50.42334, lng: 30.39264 }), source: 'osm', accuracy: 'neighborhood', accuracyM: 2200,
+    osm: Object.freeze({ type: 'node', id: 1708086258 }), wikidataId: 'Q4094861',
+  }),
+  Object.freeze({
+    id: 'ua:kyiv:microdistrict:pivdenna-borshchahivka',
+    type: 'microdistrict', country: 'UA', canonicalName: 'Південна Борщагівка', parentId: 'ua:kyiv',
+    center: Object.freeze({ lat: 50.41216, lng: 30.39967 }), source: 'osm', accuracy: 'neighborhood', accuracyM: 1400,
+    osm: Object.freeze({ type: 'node', id: 2962023006 }), wikidataId: 'Q4534022',
+  }),
+  Object.freeze({
+    id: 'ua:kyiv:microdistrict:akademmistechko',
+    type: 'microdistrict', country: 'UA', canonicalName: 'Академмістечко', parentId: 'ua:kyiv',
+    center: Object.freeze({ lat: 50.46672, lng: 30.36444 }), source: 'osm', accuracy: 'neighborhood', accuracyM: 1500,
+    osm: Object.freeze({ type: 'node', id: 2962022991 }), wikidataId: 'Q4059121',
+  }),
+  Object.freeze({
+    id: 'ua:kyiv:microdistrict:rembaza',
+    type: 'microdistrict', country: 'UA', canonicalName: 'Рембаза', parentId: 'ua:kyiv',
+    center: Object.freeze({ lat: 50.42264, lng: 30.69155 }), source: 'osm', accuracy: 'neighborhood', accuracyM: 1500,
+    osm: Object.freeze({ type: 'node', id: 1308283981 }), wikidataId: 'Q4393089',
+  }),
+  Object.freeze({
+    id: 'ua:kyiv:microdistrict:chervonyi-khutir',
+    type: 'microdistrict', country: 'UA', canonicalName: 'Червоний Хутір', parentId: 'ua:kyiv',
+    center: Object.freeze({ lat: 50.40908, lng: 30.67639 }), source: 'osm', accuracy: 'neighborhood', accuracyM: 1300,
+    osm: Object.freeze({ type: 'node', id: 1308284037 }), wikidataId: 'Q4239644',
+  }),
+]);
