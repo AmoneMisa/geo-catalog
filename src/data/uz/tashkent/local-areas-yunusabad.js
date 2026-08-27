3842416cd59a814d4f79428224c55f7459887a01
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-27 09:40:29 +0300

    refactor: add Yunusabad local-area module

---
+++ src/data/uz/tashkent/local-areas-yunusabad.js
@@ -0,0 +1,18 @@
+const osmLocalArea = (slug, canonicalName, lat, lng, osmType, osmId, accuracyM = 320) => ({
+  id: `uz:tashkent:local-area:${slug}`,
+  type: 'local_area',
+  country: 'UZ',
+  canonicalName,
+  parentId: 'uz:tashkent:yunusabad',
+  center: { lat, lng },
+  source: 'osm',
+  accuracy: 'neighborhood',
+  accuracyM,
+  osm: { type: osmType, id: osmId },
+});
+
+export const TASHKENT_YUNUSABAD_LOCAL_AREA_ENTITIES = Object.freeze([
+  osmLocalArea('kashgar', 'Kashgar', 41.32022, 69.27649, 'node', 1866932729, 320),
+  osmLocalArea('kiyot', 'Kiyot', 41.32538, 69.27791, 'node', 4778058865, 320),
+  osmLocalArea('minor', 'Minor', 41.33725, 69.27972, 'way', 1012743631, 360),
+]);
