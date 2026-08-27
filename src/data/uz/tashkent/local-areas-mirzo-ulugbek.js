944fb37474bd96772a6a4b08a6fad3c1542fbe51
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-27 09:40:19 +0300

    refactor: organize Mirzo-Ulugbek local areas

--- src/data/uz/tashkent/local-areas-mirzo-ulugbek.js
+++ src/data/uz/tashkent/local-areas-mirzo-ulugbek.js
@@ -1,4 +1,4 @@
-const osmLocalArea = (slug, canonicalName, lat, lng, osmId, accuracyM = 650) => ({
+const osmLocalArea = (slug, canonicalName, lat, lng, osmType, osmId, accuracyM = 650) => ({
   id: `uz:tashkent:local-area:${slug}`,
   type: 'local_area',
   country: 'UZ',
@@ -8,14 +8,16 @@ const osmLocalArea = (slug, canonicalName, lat, lng, osmId, accuracyM = 650) => 
   source: 'osm',
   accuracy: 'neighborhood',
   accuracyM,
-  osm: { type: 'node', id: osmId },
+  osm: { type: osmType, id: osmId },
 });
 
 export const TASHKENT_MIRZO_ULUGBEK_LOCAL_AREA_ENTITIES = Object.freeze([
-  osmLocalArea('karasu-1', 'Karasu-1', 41.33717, 69.37403, 1868229636),
-  osmLocalArea('karasu-2', 'Karasu-2', 41.33194, 69.36380, 1868229637),
-  osmLocalArea('karasu-3', 'Karasu-3', 41.33252, 69.36891, 1868229638),
-  osmLocalArea('karasu-4', 'Karasu-4', 41.33160, 69.37323, 1868229639),
-  osmLocalArea('ttz-1', 'TTZ-1', 41.35890, 69.39140, 1868216233),
-  osmLocalArea('ttz-2', 'TTZ-2', 41.35480, 69.38302, 7492673045),
+  osmLocalArea('karasu-1', 'Karasu-1', 41.33717, 69.37403, 'node', 1868229636, 650),
+  osmLocalArea('karasu-2', 'Karasu-2', 41.33194, 69.36380, 'node', 1868229637, 650),
+  osmLocalArea('karasu-3', 'Karasu-3', 41.33252, 69.36891, 'node', 1868229638, 650),
+  osmLocalArea('karasu-4', 'Karasu-4', 41.33160, 69.37323, 'node', 1868229639, 650),
+  osmLocalArea('ttz-1', 'TTZ-1', 41.35890, 69.39140, 'node', 1868216233, 650),
+  osmLocalArea('ttz-2', 'TTZ-2', 41.35480, 69.38302, 'node', 7492673045, 650),
+  osmLocalArea('ttz-4', 'TTZ-4', 41.36259, 69.38818, 'node', 1868216234, 620),
+  osmLocalArea('alay', 'Alay', 41.31732, 69.28738, 'node', 1866932727, 300),
 ]);
