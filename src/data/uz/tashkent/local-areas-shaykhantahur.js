be902b07d5d2d3efc4f2a8b273444ee6220ccf47
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-27 09:40:04 +0300

    refactor: organize Shaykhantahur local areas

--- src/data/uz/tashkent/local-areas-shaykhantahur.js
+++ src/data/uz/tashkent/local-areas-shaykhantahur.js
@@ -13,4 +13,7 @@ const osmLocalArea = (slug, canonicalName, lat, lng, osmType, osmId, accuracyM =
 
 export const TASHKENT_SHAYKHANTAHUR_LOCAL_AREA_ENTITIES = Object.freeze([
   osmLocalArea('labzak', 'Labzak', 41.32713, 69.25956, 'node', 1866901281, 650),
+  osmLocalArea('hadra', 'Khadra', 41.32409, 69.24911, 'node', 1868488533, 280),
+  osmLocalArea('jangoh', 'Jangoh', 41.32882, 69.25248, 'node', 1866901280, 280),
+  osmLocalArea('karatash', 'Karatash', 41.31722, 69.23683, 'node', 4706446883, 320),
 ]);
