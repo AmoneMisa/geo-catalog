e31c9a3c89214db2cc9537b822dfd90fea8d19fc
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-28 18:43:42 +0300

    feat(tashkent): model Tashkent City as development area

---
+++ src/data/uz/tashkent/development-areas.js
@@ -0,0 +1,14 @@
+export const TASHKENT_DEVELOPMENT_AREA_ENTITIES = Object.freeze([
+  Object.freeze({
+    id: 'uz:tashkent:development-area:tashkent-city',
+    type: 'development_area',
+    country: 'UZ',
+    canonicalName: 'Tashkent City',
+    parentId: 'uz:tashkent',
+    center: Object.freeze({ lat: 41.315101, lng: 69.250907 }),
+    source: 'osm',
+    accuracy: 'neighborhood',
+    accuracyM: 1100,
+    osm: Object.freeze({ type: 'way', id: 547234535 }),
+  }),
+]);
