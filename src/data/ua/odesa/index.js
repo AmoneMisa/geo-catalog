89df97ad0e73efc7884cf4d544912ff250070753
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-28 21:09:16 +0300

    refactor(odesa): aggregate semantic microdistrict module

--- src/data/ua/odesa/index.js
+++ src/data/ua/odesa/index.js
@@ -1,7 +1,9 @@
 import { UA_ODESA_DISTRICT_ENTITIES } from './districts.js';
+import { UA_ODESA_MICRODISTRICT_ENTITIES } from './microdistricts.js';
 import { UA_ODESA_CORE_ENTITIES } from './anchors.js';
 
 export const UA_ODESA_ENTITIES = Object.freeze([
   ...UA_ODESA_DISTRICT_ENTITIES,
+  ...UA_ODESA_MICRODISTRICT_ENTITIES,
   ...UA_ODESA_CORE_ENTITIES,
 ]);
