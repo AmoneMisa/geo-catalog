41c8c86d58cb5f0a2fc81c19c27bb6411ab62c06
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-29 01:01:39 +0300

    data(ua): replace Mykolaiv manual neighborhood points

--- src/data/ua/mykolaiv/neighborhoods.js
+++ src/data/ua/mykolaiv/neighborhoods.js
@@ -16,8 +16,8 @@ export const UA_MYKOLAIV_NEIGHBORHOOD_ENTITIES = Object.freeze([
   neighborhood('sukhyi-fontan', 'Sukhyi Fontan', 46.97858, 31.96717, 1200, { osm: Object.freeze({ type: 'node', id: 1962892059 }) }),
   neighborhood('matviivka', 'Matviivka', 47.01468, 31.92356, 1800, { osm: Object.freeze({ type: 'node', id: 1915924792 }), wikidataId: 'Q4284589' }),
   neighborhood('soliani', 'Soliani', 46.99969, 31.9973, 1400, { osm: Object.freeze({ type: 'node', id: 1979284809 }), wikidataId: 'Q4428607' }),
-  neighborhood('temvod', 'Temvod', 46.98333, 32, 1800, { source: 'manual', sourceUrl: 'https://mapcarta.com/13741034' }),
+  neighborhood('temvod', 'Temvod', 46.98184, 32.00603, 1200, { osm: Object.freeze({ type: 'node', id: 1979458687 }), wikidataId: 'Q12160438' }),
   neighborhood('viiskova-slobidka', 'Viiskova Slobidka', 46.97582, 32.02694, 1200, { osm: Object.freeze({ type: 'node', id: 1962656673 }) }),
   neighborhood('staryi-vodopii', 'Staryi Vodopii', 46.96664, 32.05294, 1300, { osm: Object.freeze({ type: 'node', id: 1979193765 }), wikidataId: 'Q4441027' }),
-  neighborhood('shyroka-balka', 'Shyroka Balka', 46.91657, 32.04192, 1700, { source: 'manual', sourceUrl: 'https://mapcarta.com/13733796' }),
+  neighborhood('shyroka-balka', 'Shyroka Balka', 46.91804, 32.04529, 1700, { osm: Object.freeze({ type: 'node', id: 1980719286 }), wikidataId: 'Q12171897' }),
 ]);
