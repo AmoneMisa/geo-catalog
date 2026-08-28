daedad247fa765e58cfde725622ddf7f74e4720c
WhitesLove <kubai.rita5@gmail.com>
2026-08-28 23:52:38 +0300

    feat: expand Kharkiv metro and Samarkand areas

--- src/data/ua/kharkiv/index.js
+++ src/data/ua/kharkiv/index.js
@@ -1,9 +1,11 @@
 import { UA_KHARKIV_POI_ENTITIES } from './poi.js';
 import { UA_KHARKIV_DISTRICT_ENTITIES } from './districts.js';
 import { UA_KHARKIV_NEIGHBORHOOD_ENTITIES } from './neighborhoods.js';
+import { UA_KHARKIV_METRO_ENTITIES } from './metro.js';
 
 export const UA_KHARKIV_ENTITIES = Object.freeze([
   ...UA_KHARKIV_DISTRICT_ENTITIES,
   ...UA_KHARKIV_NEIGHBORHOOD_ENTITIES,
+  ...UA_KHARKIV_METRO_ENTITIES,
   ...UA_KHARKIV_POI_ENTITIES,
 ]);
--- src/data/uz/samarkand/areas.js
+++ src/data/uz/samarkand/areas.js
@@ -16,4 +16,6 @@ export const SAMARKAND_AREA_ENTITIES = Object.freeze([
   spatial('motrid', 'Motrid', 'local_area', 39.68059, 66.96502, 'node', 2406821821, 900),
   spatial('kimyogarlar', 'Kimyogarlar', 'settlement', 39.67138, 66.84918, 'node', 10728273615, 1200),
   spatial('qorasuv', 'Qorasuv', 'local_area', 39.71842, 66.93132, 'way', 742858620, 650),
+  spatial('sogdiana', 'Sogdiana', 'mahalla', 39.647951, 66.960270, 'node', 11985323303, 700),
+  spatial('bogishamol', 'Bogishamol', 'local_area', 39.667063, 66.931975, 'way', 167293777, 900),
 ]);
