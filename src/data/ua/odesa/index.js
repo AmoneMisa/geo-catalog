a0fba933d7c5ed5bea46d6288f17875cb17d71d6
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-28 20:17:21 +0300

    refactor(odesa): use semantic entity export name

--- src/data/ua/odesa/index.js
+++ src/data/ua/odesa/index.js
@@ -1,7 +1,7 @@
 import { UA_ODESA_DISTRICT_ENTITIES } from './districts.js';
-import { UA_ODESA_ANCHORS } from './anchors.js';
+import { UA_ODESA_CORE_ENTITIES } from './anchors.js';
 
 export const UA_ODESA_ENTITIES = Object.freeze([
   ...UA_ODESA_DISTRICT_ENTITIES,
-  ...UA_ODESA_ANCHORS,
+  ...UA_ODESA_CORE_ENTITIES,
 ]);
