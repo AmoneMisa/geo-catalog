0b8dd7dbb53e56b1b808765eee8a2f5b842e14ce
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-30 00:54:23 +0300

    feat: add verified Denov Arboretum anchor

--- src/data/uz/tail-poi-anchors.js
+++ src/data/uz/tail-poi-anchors.js
@@ -31,6 +31,7 @@ export const UZ_TAIL_POI_ANCHORS = Object.freeze([
   osmPoi('asaka', 'uzauto-motors', 'UzAuto Motors', 40.65743, 72.23151, 'way', 27013443, 220),
   osmPoi('khojeyli', 'bazaar', 'Bazaar', 42.41644, 59.44133, 'way', 1058048372, 140),
   osmPoi('yangiyol', 'bazaar', 'Bazaar', 41.11684, 69.05468, 'way', 167322899, 140),
+  osmPoi('denov', 'denov-arboretum', 'Denov Arboretum', 38.27000, 67.89470, 'way', 621632351, 220),
   pointPoi('chartak', 'chartak-sanatorium', 'Chartak Sanatorium', 41.12665996, 71.79000821, 'official', 180),
   pointPoi('chartak', 'market', 'Market', 41.0740704, 71.8201754, 'manual', 260),
   pointPoi('denov', 'denov-bazaar', 'Denov Bazaar', 38.248681, 67.905277, 'official', 180),
