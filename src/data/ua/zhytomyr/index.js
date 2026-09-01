732146a86310ae66eb05adfbd0398fff300c221c
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-01 10:40:40 +0300

    feat(zhytomyr): register landmark anchors

--- src/data/ua/zhytomyr/index.js
+++ src/data/ua/zhytomyr/index.js
@@ -1,7 +1,9 @@
 import { UA_ZHYTOMYR_DISTRICT_ENTITIES } from './districts.js';
 import { UA_ZHYTOMYR_NEIGHBORHOOD_ENTITIES } from './neighborhoods.js';
+import { UA_ZHYTOMYR_POI_ENTITIES } from './poi.js';
 
 export const UA_ZHYTOMYR_ENTITIES = Object.freeze([
   ...UA_ZHYTOMYR_DISTRICT_ENTITIES,
   ...UA_ZHYTOMYR_NEIGHBORHOOD_ENTITIES,
+  ...UA_ZHYTOMYR_POI_ENTITIES,
 ]);
