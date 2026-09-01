6ace4bb233ad609eecd0622ff214cdc3527bb854
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-01 10:13:45 +0300

    feat(poltava): register district layer

--- src/data/ua/poltava/index.js
+++ src/data/ua/poltava/index.js
@@ -1,7 +1,9 @@
+import { UA_POLTAVA_DISTRICT_ENTITIES } from './districts.js';
 import { UA_POLTAVA_NEIGHBORHOOD_ENTITIES } from './neighborhoods.js';
 import { UA_POLTAVA_POI_ENTITIES } from './poi.js';
 
 export const UA_POLTAVA_ENTITIES = Object.freeze([
+  ...UA_POLTAVA_DISTRICT_ENTITIES,
   ...UA_POLTAVA_NEIGHBORHOOD_ENTITIES,
   ...UA_POLTAVA_POI_ENTITIES,
 ]);
