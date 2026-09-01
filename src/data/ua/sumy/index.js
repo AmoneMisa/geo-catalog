d19b6ad3239f354597c37ffa68c7fe7d51359a37
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-01 10:29:26 +0300

    feat(districts): register Sumy districts

--- src/data/ua/sumy/index.js
+++ src/data/ua/sumy/index.js
@@ -1,8 +1,10 @@
+import { UA_SUMY_DISTRICT_ENTITIES } from './districts.js';
 import { UA_SUMY_NEIGHBORHOOD_ENTITIES } from './neighborhoods.js';
 import { UA_SUMY_POI_ENTITIES } from './poi.js';
 import { UA_SUMY_RESIDENTIAL_COMPLEX_ENTITIES } from './residential-complexes.js';
 
 export const UA_SUMY_ENTITIES = Object.freeze([
+  ...UA_SUMY_DISTRICT_ENTITIES,
   ...UA_SUMY_NEIGHBORHOOD_ENTITIES,
   ...UA_SUMY_POI_ENTITIES,
   ...UA_SUMY_RESIDENTIAL_COMPLEX_ENTITIES,
