d3d9064a5ec7f70d96dde211bcd9b2087bdf58cc
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-01 23:39:49 +0300

    feat: add Osh and Karakol city anchors

--- src/data/kg/cities.js
+++ src/data/kg/cities.js
@@ -1,13 +1,17 @@
+const city = (id, canonicalName, lat, lng, sourceUrl, accuracyM = 10000) => Object.freeze({
+  id,
+  type: 'city',
+  country: 'KG',
+  canonicalName,
+  center: Object.freeze({ lat, lng }),
+  source: 'geonames',
+  sourceUrl,
+  accuracy: 'city',
+  accuracyM,
+});
+
 export const KG_CITY_ENTITIES = Object.freeze([
-  Object.freeze({
-    id: 'kg:bishkek',
-    type: 'city',
-    country: 'KG',
-    canonicalName: 'Bishkek',
-    center: Object.freeze({ lat: 42.87, lng: 74.59 }),
-    source: 'geonames',
-    sourceUrl: 'https://www.geonames.org/advanced-search.html?country=KG&q=Bishkek',
-    accuracy: 'city',
-    accuracyM: 10000,
-  }),
+  city('kg:bishkek', 'Bishkek', 42.87, 74.59, 'https://www.geonames.org/advanced-search.html?country=KG&q=Bishkek'),
+  city('kg:osh', 'Osh', 40.53, 72.80, 'https://www.geonames.org/advanced-search.html?country=KG&q=Osh'),
+  city('kg:karakol', 'Karakol', 42.49, 78.39, 'https://www.geonames.org/advanced-search.html?country=KG&q=Karakol'),
 ]);
