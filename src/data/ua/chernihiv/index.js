c7b1b44617901e29b8a10f6bdcd4da485cb99198
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-01 10:13:57 +0300

    feat(chernihiv): register district layer

--- src/data/ua/chernihiv/index.js
+++ src/data/ua/chernihiv/index.js
@@ -1,7 +1,9 @@
+import { UA_CHERNIHIV_DISTRICT_ENTITIES } from './districts.js';
 import { UA_CHERNIHIV_NEIGHBORHOOD_ENTITIES } from './neighborhoods.js';
 import { UA_CHERNIHIV_POI_ENTITIES } from './poi.js';
 
 export const UA_CHERNIHIV_ENTITIES = Object.freeze([
+  ...UA_CHERNIHIV_DISTRICT_ENTITIES,
   ...UA_CHERNIHIV_NEIGHBORHOOD_ENTITIES,
   ...UA_CHERNIHIV_POI_ENTITIES,
 ]);
