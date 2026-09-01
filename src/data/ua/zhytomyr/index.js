9d919f12e6af8eb9314e8a5d393fc2ea2031e712
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-01 10:37:59 +0300

    feat(zhytomyr): register neighborhood anchors

--- src/data/ua/zhytomyr/index.js
+++ src/data/ua/zhytomyr/index.js
@@ -1,5 +1,7 @@
 import { UA_ZHYTOMYR_DISTRICT_ENTITIES } from './districts.js';
+import { UA_ZHYTOMYR_NEIGHBORHOOD_ENTITIES } from './neighborhoods.js';
 
 export const UA_ZHYTOMYR_ENTITIES = Object.freeze([
   ...UA_ZHYTOMYR_DISTRICT_ENTITIES,
+  ...UA_ZHYTOMYR_NEIGHBORHOOD_ENTITIES,
 ]);
