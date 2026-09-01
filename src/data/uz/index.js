507845394d172ed84b20da4712dd17ec9164556e
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-02 00:29:18 +0300

    fix: include Angren spatial modules

--- src/data/uz/index.js
+++ src/data/uz/index.js
@@ -10,6 +10,8 @@ import { NUKUS_ENTITIES } from './cities/nukus.js';
 import { URGENCH_ENTITIES } from './cities/urgench.js';
 import { TURTKUL_ENTITIES } from './cities/turtkul.js';
 import { ALMALYK_MICRODISTRICT_ENTITIES } from './almalyk-microdistricts.js';
+import { ANGREN_QUARTER_ENTITIES } from './angren-quarters.js';
+import { ANGREN_STREET_ENTITIES } from './angren-streets.js';
 import { UZ_SECONDARY_CITY_ANCHORS } from './secondary-city-anchors.js';
 import { UZ_SECONDARY_AREA_ADDITIONS } from './secondary-area-additions.js';
 import { UZ_SECONDARY_LOCALITY_ENTITIES } from './secondary-localities.js';
@@ -35,6 +37,8 @@ export const UZ_ENTITIES = Object.freeze([
   ...URGENCH_ENTITIES,
   ...TURTKUL_ENTITIES,
   ...ALMALYK_MICRODISTRICT_ENTITIES,
+  ...ANGREN_QUARTER_ENTITIES,
+  ...ANGREN_STREET_ENTITIES,
   ...UZ_SECONDARY_CITY_ANCHORS,
   ...UZ_SECONDARY_AREA_ADDITIONS,
   ...UZ_SECONDARY_LOCALITY_ENTITIES,
