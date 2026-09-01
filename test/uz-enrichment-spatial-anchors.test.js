6712b3e2c09a864cb7abe68f98b6f66da9f25b5e
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-01 23:19:39 +0300

    test: extend UZ enrichment anchor coverage

--- test/uz-enrichment-spatial-anchors.test.js
+++ test/uz-enrichment-spatial-anchors.test.js
@@ -3,6 +3,10 @@ import assert from 'node:assert/strict';
 import { resolveLexiconGeoEntity } from '../src/lexicon-bridge.js';
 
 const expected = Object.freeze([
+  ['Andijan', 'mahalla', "Ma'rifat", 20598677],
+  ['Andijan', 'mahalla', 'Mustaqillikning 21 yilligi', 20582797],
+  ['Andijan', 'mahalla', "Taxtako'prik", 20515956],
+  ['Fergana', 'local_area', 'Qirguli', 2171217263],
   ['Namangan', 'mahalla', 'Porloq', 20587979],
   ['Qarshi', 'mahalla', 'Navo', 1027278316],
   ['Qarshi', 'mahalla', 'Gungon', 1027317833],
@@ -11,6 +15,7 @@ const expected = Object.freeze([
   ['Jizzakh', 'local_area', 'Zilol', 11725490815],
   ['Navoiy', 'mahalla', 'Guliston', 1137853275],
   ['Xonobod', 'poi', 'Andijan Reservoir', 14663093],
+  ['Kattakurgan', 'poi', 'Kattakurgan Reservoir', 12571708],
 ]);
 
 test('verified enrichment anchors resolve through the lexicon bridge', () => {
