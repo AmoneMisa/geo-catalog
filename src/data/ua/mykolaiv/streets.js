73b180099716ff2cbf71249ac5886387691c9bc3
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-02 15:27:12 +0300

    feat: retain Mykolaiv Levanevtsiv street discovery

---
+++ src/data/ua/mykolaiv/streets.js
@@ -0,0 +1,14 @@
+export const UA_MYKOLAIV_STREET_ENTITIES = Object.freeze([
+  Object.freeze({
+    id: 'ua:mykolaiv:street:levanevtsiv',
+    type: 'street',
+    country: 'UA',
+    canonicalName: 'Levanevtsiv Street',
+    parentId: 'ua:mykolaiv',
+    center: Object.freeze({ lat: 46.9630059, lng: 31.9455963 }),
+    source: 'osm',
+    accuracy: 'street',
+    accuracyM: 1100,
+    osm: Object.freeze({ type: 'way', id: 185645173 }),
+  }),
+]);
