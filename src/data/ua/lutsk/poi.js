c22bfab63be91c45d706b2aaf0fdbafba5110ff8
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-02 19:08:19 +0300

    feat: add Lutsk Lesia Ukrainka Park owner

---
+++ src/data/ua/lutsk/poi.js
@@ -0,0 +1,14 @@
+export const UA_LUTSK_POI_ENTITIES = Object.freeze([
+  Object.freeze({
+    id: 'ua:lutsk:poi:lesia-ukrainka-park',
+    type: 'poi.park',
+    country: 'UA',
+    canonicalName: 'Lesia Ukrainka Park',
+    parentId: 'ua:lutsk',
+    center: Object.freeze({ lat: 50.741263, lng: 25.3274449 }),
+    source: 'osm',
+    accuracy: 'poi',
+    accuracyM: 650,
+    osm: Object.freeze({ type: 'relation', id: 9755771 }),
+  }),
+]);
