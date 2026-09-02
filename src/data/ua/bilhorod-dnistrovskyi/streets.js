fd33d3639e43a894b0e801bf1fe39113dd3ef3df
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-02 13:44:08 +0300

    feat: retain Bilhorod Portovyi street discovery

---
+++ src/data/ua/bilhorod-dnistrovskyi/streets.js
@@ -0,0 +1,14 @@
+export const UA_BILHOROD_DNISTROVSKYI_STREET_ENTITIES = Object.freeze([
+  Object.freeze({
+    id: 'ua:bilhorod-dnistrovskyi:street:portovyi-lane',
+    type: 'street',
+    country: 'UA',
+    canonicalName: 'Portovyi Lane',
+    parentId: 'ua:bilhorod-dnistrovskyi',
+    center: Object.freeze({ lat: 46.1871724, lng: 30.3512343 }),
+    source: 'osm',
+    accuracy: 'street',
+    accuracyM: 450,
+    osm: Object.freeze({ type: 'way', id: 413109017 }),
+  }),
+]);
