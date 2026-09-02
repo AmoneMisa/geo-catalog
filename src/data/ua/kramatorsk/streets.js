1de3f01b757da43fb05a4fa61cefa3a971a763f8
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-02 15:18:55 +0300

    feat: retain Kramatorsk Parkova street discovery

---
+++ src/data/ua/kramatorsk/streets.js
@@ -0,0 +1,14 @@
+export const UA_KRAMATORSK_STREET_ENTITIES = Object.freeze([
+  Object.freeze({
+    id: 'ua:kramatorsk:street:parkova',
+    type: 'street',
+    country: 'UA',
+    canonicalName: 'Parkova Street',
+    parentId: 'ua:kramatorsk',
+    center: Object.freeze({ lat: 48.7206531, lng: 37.6149466 }),
+    source: 'osm',
+    accuracy: 'street',
+    accuracyM: 1400,
+    osm: Object.freeze({ type: 'way', id: 335788767 }),
+  }),
+]);
