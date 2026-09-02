71af771f7afba72df958f42efd34f2946d4cc45a
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-02 15:20:18 +0300

    feat: add Sloviansk railway station owner

---
+++ src/data/ua/sloviansk/poi.js
@@ -0,0 +1,14 @@
+export const UA_SLOVIANSK_POI_ENTITIES = Object.freeze([
+  Object.freeze({
+    id: 'ua:sloviansk:poi:railway-station',
+    type: 'poi.railway_station',
+    country: 'UA',
+    canonicalName: 'Railway Station',
+    parentId: 'ua:sloviansk',
+    center: Object.freeze({ lat: 48.8317443, lng: 37.5600303 }),
+    source: 'osm',
+    accuracy: 'poi',
+    accuracyM: 180,
+    osm: Object.freeze({ type: 'way', id: 127089884 }),
+  }),
+]);
