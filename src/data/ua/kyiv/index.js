ce48f1ff81fb21c4e838f036827d9c00d9735e02
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-02 19:05:50 +0300

    feat: include Kyiv metro and street report owners

--- src/data/ua/kyiv/index.js
+++ src/data/ua/kyiv/index.js
@@ -2,10 +2,14 @@ import { UA_KYIV_ANCHORS } from './anchors.js';
 import { UA_KYIV_NEIGHBORHOODS } from './neighborhoods.js';
 import { UA_KYIV_RESIDENTIAL_COMPLEX_ENTITIES } from './residential-complexes.js';
 import { UA_KYIV_POI_ENTITIES } from './poi.js';
+import { UA_KYIV_METRO_ENTITIES } from './metro.js';
+import { UA_KYIV_STREET_ENTITIES } from './streets.js';
 
 export const UA_KYIV_ENTITIES = Object.freeze([
   ...UA_KYIV_ANCHORS,
   ...UA_KYIV_NEIGHBORHOODS,
   ...UA_KYIV_RESIDENTIAL_COMPLEX_ENTITIES,
   ...UA_KYIV_POI_ENTITIES,
+  ...UA_KYIV_METRO_ENTITIES,
+  ...UA_KYIV_STREET_ENTITIES,
 ]);
