cc56595ee94a9006b38f33b5b121136534f6bcdb
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-02 14:31:01 +0300

    feat: aggregate Uzhhorod geo modules

---
+++ src/data/ua/uzhhorod/index.js
@@ -0,0 +1,7 @@
+import { UA_UZHHOROD_NEIGHBORHOOD_ENTITIES } from './neighborhoods.js';
+import { UA_UZHHOROD_POI_ENTITIES } from './poi.js';
+
+export const UA_UZHHOROD_ENTITIES = Object.freeze([
+  ...UA_UZHHOROD_NEIGHBORHOOD_ENTITIES,
+  ...UA_UZHHOROD_POI_ENTITIES,
+]);
