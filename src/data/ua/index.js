2a7dcd160fc38e47afd07a9b8e5c8be601d93ca8
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-03 02:36:50 +0300

    feat(ua): register Drohobych city layer

--- src/data/ua/index.js
+++ src/data/ua/index.js
@@ -11,6 +11,7 @@ import { UA_KREMENCHUK_ENTITIES } from './kremenchuk/index.js';
 import { UA_BILA_TSERKVA_ENTITIES } from './bila-tserkva/index.js';
 import { UA_UMAN_ENTITIES } from './uman/index.js';
 import { UA_KAMIANETS_PODILSKYI_ENTITIES } from './kamianets-podilskyi/index.js';
+import { UA_DROHOBYCH_ENTITIES } from './drohobych/index.js';
 import { UA_BERDIANSK_ENTITIES } from './berdiansk/index.js';
 import { UA_BERDYCHIV_ENTITIES } from './berdychiv/index.js';
 import { UA_BILHOROD_DNISTROVSKYI_ENTITIES } from './bilhorod-dnistrovskyi/index.js';
@@ -48,6 +49,7 @@ export const UA_ENTITIES = Object.freeze([
   ...UA_BILA_TSERKVA_ENTITIES,
   ...UA_UMAN_ENTITIES,
   ...UA_KAMIANETS_PODILSKYI_ENTITIES,
+  ...UA_DROHOBYCH_ENTITIES,
   ...UA_BERDIANSK_ENTITIES,
   ...UA_BERDYCHIV_ENTITIES,
   ...UA_BILHOROD_DNISTROVSKYI_ENTITIES,
