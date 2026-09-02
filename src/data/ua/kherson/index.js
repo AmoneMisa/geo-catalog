b6b312c900d02b3d2f2c8dbe393f3c959ec9017e
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-02 18:59:20 +0300

    feat: include Kherson residential enrichment

--- src/data/ua/kherson/index.js
+++ src/data/ua/kherson/index.js
@@ -1,9 +1,11 @@
 import { UA_KHERSON_DISTRICT_ENTITIES } from './districts.js';
 import { UA_KHERSON_NEIGHBORHOOD_ENTITIES } from './neighborhoods.js';
 import { UA_KHERSON_POI_ENTITIES } from './poi.js';
+import { UA_KHERSON_RESIDENTIAL_COMPLEX_ENTITIES } from './residential-complexes.js';
 
 export const UA_KHERSON_ENTITIES = Object.freeze([
   ...UA_KHERSON_DISTRICT_ENTITIES,
   ...UA_KHERSON_NEIGHBORHOOD_ENTITIES,
   ...UA_KHERSON_POI_ENTITIES,
+  ...UA_KHERSON_RESIDENTIAL_COMPLEX_ENTITIES,
 ]);
