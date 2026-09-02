726ea1c45aba35fa670f31adde6f0d93bb0cf8d6
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-02 18:58:50 +0300

    feat: add Kherson report-derived neighborhood owners

--- src/data/ua/kherson/neighborhoods.js
+++ src/data/ua/kherson/neighborhoods.js
@@ -12,12 +12,14 @@ const neighborhood = (slug, canonicalName, lat, lng, accuracyM, extra = {}) => O
 });
 
 export const UA_KHERSON_NEIGHBORHOOD_ENTITIES = Object.freeze([
+  neighborhood('tsentr', 'Tsentr', 46.6386597, 32.612576, 1500, { osm: Object.freeze({ type: 'node', id: 9402413514 }) }),
   neighborhood('tavriiskyi', 'Tavriiskyi', 46.672, 32.61752, 1800, { osm: Object.freeze({ type: 'node', id: 9402413508 }) }),
   neighborhood('tavriiskyi-2', 'Tavriiskyi-2', 46.67922, 32.62012, 1100, { osm: Object.freeze({ type: 'node', id: 9402413504 }) }),
   neighborhood('tavriiskyi-3', 'Tavriiskyi-3', 46.66707, 32.61651, 1100, { osm: Object.freeze({ type: 'node', id: 9402413506 }) }),
   neighborhood('tavriiskyi-4', 'Tavriiskyi-4', 46.66919, 32.62385, 1100, { osm: Object.freeze({ type: 'node', id: 9402413507 }) }),
   neighborhood('pivnichnyi', 'Pivnichnyi', 46.67696, 32.60189, 1500, { osm: Object.freeze({ type: 'node', id: 9402413483 }) }),
   neighborhood('shumenskyi', 'Shumenskyi', 46.64598, 32.56415, 1700, { osm: Object.freeze({ type: 'node', id: 9394331056 }) }),
+  neighborhood('khbk', 'KhBK', 46.6625935, 32.6435207, 1500, { osm: Object.freeze({ type: 'relation', id: 4487768 }) }),
   neighborhood('zhytloselyshche', 'Zhytloselyshche', 46.64626, 32.58878, 1400, { osm: Object.freeze({ type: 'node', id: 8632583884 }) }),
   neighborhood('ostriv', 'Ostriv', 46.619, 32.58271, 1700, { osm: Object.freeze({ type: 'node', id: 8635895888 }) }),
   neighborhood('voienka', 'Voienka', 46.64634, 32.63856, 1200, { osm: Object.freeze({ type: 'node', id: 8632583883 }) }),
