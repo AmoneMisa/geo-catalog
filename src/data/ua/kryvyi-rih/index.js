741d3e80e3940d8550d2224b4db319c34346200f
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-02 19:00:01 +0300

    feat: aggregate Kryvyi Rih enrichment modules

--- src/data/ua/kryvyi-rih/index.js
+++ src/data/ua/kryvyi-rih/index.js
@@ -1,2 +1,9 @@
 import { UA_KRYVYI_RIH_DISTRICT_ENTITIES } from './districts.js';
-export const UA_KRYVYI_RIH_ENTITIES = UA_KRYVYI_RIH_DISTRICT_ENTITIES;
+import { UA_KRYVYI_RIH_NEIGHBORHOOD_ENTITIES } from './neighborhoods.js';
+import { UA_KRYVYI_RIH_POI_ENTITIES } from './poi.js';
+
+export const UA_KRYVYI_RIH_ENTITIES = Object.freeze([
+  ...UA_KRYVYI_RIH_DISTRICT_ENTITIES,
+  ...UA_KRYVYI_RIH_NEIGHBORHOOD_ENTITIES,
+  ...UA_KRYVYI_RIH_POI_ENTITIES,
+]);
