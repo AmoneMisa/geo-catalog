132735f77ed48bab833fe83a82acb02dfcdbe24a
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-02 14:30:27 +0300

    feat: aggregate Ternopil geo modules

---
+++ src/data/ua/ternopil/index.js
@@ -0,0 +1,7 @@
+import { UA_TERNOPIL_NEIGHBORHOOD_ENTITIES } from './neighborhoods.js';
+import { UA_TERNOPIL_POI_ENTITIES } from './poi.js';
+
+export const UA_TERNOPIL_ENTITIES = Object.freeze([
+  ...UA_TERNOPIL_NEIGHBORHOOD_ENTITIES,
+  ...UA_TERNOPIL_POI_ENTITIES,
+]);
