28d92d93b8b5fb508831176fd36ff8537172350d
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-04 01:39:10 +0300

    test(ua): align Sumy Airport source

--- test/sumy-airport.test.js
+++ test/sumy-airport.test.js
@@ -10,8 +10,8 @@ test('Sumy exposes verified airport anchor and aviation codes', () => {
   assert.equal(airport.type, 'poi.airport');
   assert.equal(airport.canonicalName, 'Sumy Airport');
   assert.equal(airport.parentId, 'ua:sumy');
-  assert.deepEqual(airport.center, { lat: 50.8583333, lng: 34.7627778 });
+  assert.deepEqual(airport.center, { lat: 50.8583333, lng: 34.7625 });
   assert.equal(airport.iataCode, 'UMY');
   assert.equal(airport.icaoCode, 'UKHS');
-  assert.equal(airport.officialUrl, 'https://www.aisukraine.net/');
+  assert.equal(airport.sourceUrl, 'https://avia.gov.ua/placemarks/oblasne-komunalne-pidpriyemstvo-aeroport-sumi/');
 });
