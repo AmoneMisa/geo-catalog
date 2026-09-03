e525dad05d82d62ca635dfc23785aa5a9be6ce1b
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-03 04:32:08 +0300

    feat(ua): register Kovel entities

--- src/data/ua/index.js
+++ src/data/ua/index.js
@@ -13,6 +13,7 @@ import { UA_UMAN_ENTITIES } from './uman/index.js';
 import { UA_KAMIANETS_PODILSKYI_ENTITIES } from './kamianets-podilskyi/index.js';
 import { UA_DROHOBYCH_ENTITIES } from './drohobych/index.js';
 import { UA_KOLOMYIA_ENTITIES } from './kolomyia/index.js';
+import { UA_KOVEL_ENTITIES } from './kovel/index.js';
 import { UA_BERDIANSK_ENTITIES } from './berdiansk/index.js';
 import { UA_BERDYCHIV_ENTITIES } from './berdychiv/index.js';
 import { UA_BILHOROD_DNISTROVSKYI_ENTITIES } from './bilhorod-dnistrovskyi/index.js';
@@ -52,6 +53,7 @@ export const UA_ENTITIES = Object.freeze([
   ...UA_KAMIANETS_PODILSKYI_ENTITIES,
   ...UA_DROHOBYCH_ENTITIES,
   ...UA_KOLOMYIA_ENTITIES,
+  ...UA_KOVEL_ENTITIES,
   ...UA_BERDIANSK_ENTITIES,
   ...UA_BERDYCHIV_ENTITIES,
   ...UA_BILHOROD_DNISTROVSKYI_ENTITIES,
