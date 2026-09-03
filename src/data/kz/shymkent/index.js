9d3c92ab8af1617bbce0c98854dfdbdf79f7d158
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-03 21:53:23 +0300

    feat(kz): expose Shymkent POI layer

--- src/data/kz/shymkent/index.js
+++ src/data/kz/shymkent/index.js
@@ -1,5 +1,7 @@
 import { KZ_SHYMKENT_DISTRICT_ENTITIES } from './districts.js';
+import { KZ_SHYMKENT_POI_ENTITIES } from './poi.js';
 
 export const KZ_SHYMKENT_ENTITIES = Object.freeze([
   ...KZ_SHYMKENT_DISTRICT_ENTITIES,
+  ...KZ_SHYMKENT_POI_ENTITIES,
 ]);
