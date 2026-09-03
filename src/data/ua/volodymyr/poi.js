5fcd81891aed64c588e9eed1a1894a697f25f0a7
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-03 09:36:14 +0300

    feat(ua): add Volodymyr St Basil rotunda POI

--- src/data/ua/volodymyr/poi.js
+++ src/data/ua/volodymyr/poi.js
@@ -44,4 +44,15 @@ export const VOLODYMYR_POIS = Object.freeze([
     osm: Object.freeze({ type: 'way', id: 145439183 }),
     wikidataId: 'Q4478056',
   }),
+  poi(
+    'st-basil-rotunda',
+    'Свято-Василівська церква-ротонда',
+    50.841286,
+    24.320423,
+    50,
+    {
+      source: 'manual',
+      sourceUrl: 'https://commons.wikimedia.org/wiki/File:%D0%92%D0%BE%D0%BB%D0%BE%D0%B4%D0%B8%D0%BC%D0%B8%D1%80-%D0%92%D0%BE%D0%BB%D0%B8%D0%BD%D1%81%D1%8C%D0%BA%D0%B8%D0%B9_%D0%92%D0%B0%D1%81%D0%B8%D0%BB%D1%96%D0%B2%D1%81%D1%8C%D0%BA%D0%B0_%D1%86%D0%B5%D1%80%D0%BA%D0%B2%D0%B0.jpg',
+    },
+  ),
 ]);
