9f3f9ff99323ba91dd5417de02a113026f5af911
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-03 23:53:25 +0300

    feat(kz): expose Karaganda POI layer

--- src/data/kz/karaganda/index.js
+++ src/data/kz/karaganda/index.js
@@ -1,5 +1,7 @@
 import { KZ_KARAGANDA_DISTRICT_ENTITIES } from './districts.js';
+import { KZ_KARAGANDA_POI_ENTITIES } from './poi.js';
 
 export const KZ_KARAGANDA_ENTITIES = Object.freeze([
   ...KZ_KARAGANDA_DISTRICT_ENTITIES,
+  ...KZ_KARAGANDA_POI_ENTITIES,
 ]);
