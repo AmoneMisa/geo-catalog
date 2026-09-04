c2779e548a9d9b59aec8fb969d86df0db8edb34a
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-04 13:04:17 +0300

    Add verified Mon Paris residential complex in Osh

--- src/data/kg/osh/residential-complexes.js
+++ src/data/kg/osh/residential-complexes.js
@@ -1,4 +1,4 @@
-const residential = (slug, canonicalName, lat, lng, osmType, osmId, accuracyM = 260) => Object.freeze({
+const osmResidential = (slug, canonicalName, lat, lng, osmType, osmId, accuracyM = 260) => Object.freeze({
   id: `kg:osh:residential:${slug}`,
   type: 'residential_complex',
   country: 'KG',
@@ -12,6 +12,20 @@ const residential = (slug, canonicalName, lat, lng, osmType, osmId, accuracyM = 
   osm: Object.freeze({ type: osmType, id: osmId }),
 });
 
+const mappedResidential = (slug, canonicalName, lat, lng, sourceUrl, accuracyM = 220) => Object.freeze({
+  id: `kg:osh:residential:${slug}`,
+  type: 'residential_complex',
+  country: 'KG',
+  canonicalName,
+  parentId: 'kg:osh',
+  center: Object.freeze({ lat, lng }),
+  source: 'manual',
+  sourceUrl,
+  accuracy: 'building',
+  accuracyM,
+});
+
 export const KG_OSH_RESIDENTIAL_COMPLEX_ENTITIES = Object.freeze([
-  residential('asman-residence-1', 'Asman Residence 1', 40.5051476, 72.8122757, 'way', 1534183059, 320),
+  osmResidential('asman-residence-1', 'Asman Residence 1', 40.5051476, 72.8122757, 'way', 1534183059, 320),
+  mappedResidential('mon-paris', 'Mon Paris', 40.5152140, 72.8126340, 'https://2gis.kg/osh/directions/points/%7C72.812634%2C40.515214%3B70000001045267673', 180),
 ]);
