205ed8ab06e93c701c3b4a4eb87e621af631e0f8
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-04 13:59:53 +0300

    Align Odesa residential data with city architecture

---
+++ src/data/ua/odesa/residential-complexes.js
@@ -0,0 +1,23 @@
+const residential = (slug, canonicalName, lat, lng, sourceUrl) => Object.freeze({
+  id: `ua:odesa:residential-complex:${slug}`,
+  type: 'residential_complex',
+  country: 'UA',
+  canonicalName,
+  parentId: 'ua:odesa',
+  center: Object.freeze({ lat, lng }),
+  source: 'manual',
+  sourceUrl,
+  accuracy: 'building',
+  accuracyM: 280,
+});
+
+const YANDEX_ODESA_RESIDENTIAL_SEARCH = 'https://yandex.com/maps/?text=%D0%96%D0%9A%2C%20Odesa';
+
+export const UA_ODESA_RESIDENTIAL_COMPLEX_ENTITIES = Object.freeze([
+  residential('arc-palace', 'Arc Palace', 46.438772, 30.758746, YANDEX_ODESA_RESIDENTIAL_SEARCH),
+  residential('synia-ptakh', 'Synia Ptakh', 46.47375, 30.759286, YANDEX_ODESA_RESIDENTIAL_SEARCH),
+  residential('favorit', 'Favorit', 46.447069, 30.734267, YANDEX_ODESA_RESIDENTIAL_SEARCH),
+  residential('club-marine', 'Club Marine', 46.368985, 30.726713, YANDEX_ODESA_RESIDENTIAL_SEARCH),
+  residential('7-pearl', '7 Pearl', 46.436976, 30.766204, YANDEX_ODESA_RESIDENTIAL_SEARCH),
+  residential('8-pearl', '8 Pearl', 46.437747, 30.765668, YANDEX_ODESA_RESIDENTIAL_SEARCH),
+]);
