b24bf04f826cf6f074a736f50be2930efc25c36d
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-04 12:01:22 +0300

    chore(data): strengthen Dnipro residential sources

--- src/data/ua/dnipro/residential-complexes.js
+++ src/data/ua/dnipro/residential-complexes.js
@@ -31,12 +31,12 @@ export const UA_DNIPRO_RESIDENTIAL_COMPLEX_ENTITIES = Object.freeze([
   residential('west-hall', 'West Hall', 48.4285198, 35.0649704, 887478103, 320),
 
   // Strong single-source entities promoted from the cleaned page-scrape batch.
-  cleanedResidential('pikhtovyi', 'Pikhtovyi', 48.442422, 35.008002, 'https://dom.ria.com/novostroyka-zhk-pykhtovyi-3733/'),
-  cleanedResidential('dom-na-tytova', 'Dom na Tytova', 48.430389, 35.008487, YANDEX_DNIPRO_RESIDENTIAL_SEARCH),
+  cleanedResidential('pikhtovyi', 'Pikhtovyi', 48.442422, 35.008002, 'https://dom.ria.com/uk/novostroyka-zhk-pikhtovyi-3733/'),
+  cleanedResidential('dom-na-tytova', 'Dom na Tytova', 48.430389, 35.008487, 'https://dom.ria.com/novostroyka-zhk-dom-na-tytova-5323/'),
   cleanedResidential('atlant', 'Atlant', 48.465165, 35.02172, 'https://lun.ua/new/dnipro/atlant'),
   cleanedResidential('krasnopolskyi', 'Krasnopolskyi', 48.418717, 34.937757, YANDEX_DNIPRO_RESIDENTIAL_SEARCH),
   cleanedResidential('lighthouse', 'Lighthouse', 48.45787, 35.074505, 'https://korter.ua/uk/%D0%B6%D0%BA-%D0%BB%D0%B0%D0%B9%D1%82%D1%85%D0%B0%D1%83%D1%81-%D0%B4%D0%BD%D1%96%D0%BF%D1%80%D0%BE'),
-  cleanedResidential('palermo', 'Palermo', 48.464717, 35.046181, YANDEX_DNIPRO_RESIDENTIAL_SEARCH),
-  cleanedResidential('salyut', 'Salyut', 48.415154, 35.068639, YANDEX_DNIPRO_RESIDENTIAL_SEARCH),
+  cleanedResidential('palermo', 'Palermo', 48.464717, 35.046181, 'https://dom.ria.com/uk/prodazha-kvartir/dnepr-zhk-palermo-4969/'),
+  cleanedResidential('salyut', 'Salyut', 48.415154, 35.068639, 'https://daytona.com.ua/ru/objects/zhk-saljut/'),
   cleanedResidential('ptakhy', 'Ptakhy', 48.429134, 35.038105, YANDEX_DNIPRO_RESIDENTIAL_SEARCH),
 ]);
