a3f6d915e3587bf3389aded518e9dfc1bfecd369
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-04 12:53:45 +0300

    Wire Osh settlements and residential complexes

--- src/data/kg/osh/index.js
+++ src/data/kg/osh/index.js
@@ -1,5 +1,9 @@
 import { KG_OSH_MICRODISTRICT_ENTITIES } from './microdistricts.js';
+import { KG_OSH_SETTLEMENT_ENTITIES } from './settlements.js';
+import { KG_OSH_RESIDENTIAL_COMPLEX_ENTITIES } from './residential-complexes.js';
 
 export const KG_OSH_ENTITIES = Object.freeze([
   ...KG_OSH_MICRODISTRICT_ENTITIES,
+  ...KG_OSH_SETTLEMENT_ENTITIES,
+  ...KG_OSH_RESIDENTIAL_COMPLEX_ENTITIES,
 ]);
