1ef4dca009b3d5eaf8594daa2ec8a7394630c368
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-04 08:55:11 +0300

    feat(ro): add verified Cluj-Napoca core POIs

---
+++ src/data/ro/cluj-napoca/index.js
@@ -0,0 +1,5 @@
+import { RO_CLUJ_NAPOCA_POI_ENTITIES } from './poi.js';
+
+export const RO_CLUJ_NAPOCA_ENTITIES = Object.freeze([
+  ...RO_CLUJ_NAPOCA_POI_ENTITIES,
+]);
--- src/data/ro/index.js
+++ src/data/ro/index.js
@@ -1,7 +1,9 @@
 import { RO_CITY_ENTITIES } from './cities.js';
 import { RO_BUCHAREST_ENTITIES } from './bucharest/index.js';
+import { RO_CLUJ_NAPOCA_ENTITIES } from './cluj-napoca/index.js';
 
 export const RO_ENTITIES = Object.freeze([
   ...RO_CITY_ENTITIES,
   ...RO_BUCHAREST_ENTITIES,
+  ...RO_CLUJ_NAPOCA_ENTITIES,
 ]);
