31c13351bcfec916b06783c7c7451ac3c77c7281
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-04 12:46:53 +0300

    Wire Bishkek streets into catalog

--- src/data/kg/bishkek/index.js
+++ src/data/kg/bishkek/index.js
@@ -2,10 +2,12 @@ import { KG_BISHKEK_DISTRICT_ENTITIES } from './districts.js';
 import { KG_BISHKEK_MICRODISTRICT_ENTITIES } from './microdistricts.js';
 import { KG_BISHKEK_LOCAL_AREA_ENTITIES } from './local-areas.js';
 import { KG_BISHKEK_RESIDENTIAL_COMPLEX_ENTITIES } from './residential-complexes.js';
+import { KG_BISHKEK_STREET_ENTITIES } from './streets.js';
 
 export const KG_BISHKEK_ENTITIES = Object.freeze([
   ...KG_BISHKEK_DISTRICT_ENTITIES,
   ...KG_BISHKEK_MICRODISTRICT_ENTITIES,
   ...KG_BISHKEK_LOCAL_AREA_ENTITIES,
   ...KG_BISHKEK_RESIDENTIAL_COMPLEX_ENTITIES,
+  ...KG_BISHKEK_STREET_ENTITIES,
 ]);
