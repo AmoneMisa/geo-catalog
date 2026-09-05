7f8df954f96d8815e301f37cd71fd6f740698149
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-05 21:09:30 +0300

    feat: wire reviewed Kryvyi Rih streets

--- data-source/ua/kryvyi-rih/index.js
+++ data-source/ua/kryvyi-rih/index.js
@@ -1,9 +1,11 @@
 import { UA_KRYVYI_RIH_DISTRICT_ENTITIES } from './districts.js';
 import { UA_KRYVYI_RIH_NEIGHBORHOOD_ENTITIES } from './neighborhoods.js';
 import { UA_KRYVYI_RIH_POI_ENTITIES } from './poi.js';
+import { UA_KRYVYI_RIH_REVIEWED_STREET_ENTITIES } from './reviewed-streets.js';
 
 export const UA_KRYVYI_RIH_ENTITIES = Object.freeze([
   ...UA_KRYVYI_RIH_DISTRICT_ENTITIES,
   ...UA_KRYVYI_RIH_NEIGHBORHOOD_ENTITIES,
+  ...UA_KRYVYI_RIH_REVIEWED_STREET_ENTITIES,
   ...UA_KRYVYI_RIH_POI_ENTITIES,
 ]);
