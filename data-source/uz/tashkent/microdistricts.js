439528f869469fdbe205777c5c2707edcf397abd
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-05 20:00:28 +0300

    fix: remove duplicate Chilanzar-20 declaration

--- data-source/uz/tashkent/microdistricts.js
+++ data-source/uz/tashkent/microdistricts.js
@@ -21,7 +21,6 @@ export const TASHKENT_MICRODISTRICT_ENTITIES = Object.freeze([
   osmMicrodistrict('chilanzar-8', 'Chilanzar-8', 'uz:tashkent:chilanzar', 41.29096, 69.20434, 'node', 1866884786, 500),
   osmMicrodistrict('chilanzar-9', 'Chilanzar-9', 'uz:tashkent:chilanzar', 41.28828, 69.19897, 'node', 1865344356, 500),
   osmMicrodistrict('chilanzar-10', 'Chilanzar-10', 'uz:tashkent:chilanzar', 41.28281, 69.19775, 'node', 1865359942, 500),
-  osmMicrodistrict('chilanzar-20', 'Chilanzar-20', 'uz:tashkent:chilanzar', 41.2665051, 69.1798855, 'relation', 1850375, 600),
   osmMicrodistrict('karakamysh', 'Karakamysh', 'uz:tashkent:almazar', 41.364, 69.21475, 'node', 10119542204, 1100),
   osmMicrodistrict('sebzar', 'Sebzar', 'uz:tashkent:almazar', 41.33478, 69.24943, 'node', 1866097655, 650),
 ]);
