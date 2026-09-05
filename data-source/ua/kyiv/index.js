fee17e1292e7ea4d056da63e303647490dc4b54d
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-05 20:52:32 +0300

    feat: include reviewed Kyiv street

--- data-source/ua/kyiv/index.js
+++ data-source/ua/kyiv/index.js
@@ -4,6 +4,7 @@ import { UA_KYIV_RESIDENTIAL_COMPLEX_ENTITIES } from './residential-complexes.js
 import { UA_KYIV_POI_ENTITIES } from './poi.js';
 import { UA_KYIV_METRO_ENTITIES } from './metro.js';
 import { UA_KYIV_STREET_ENTITIES } from './streets.js';
+import { UA_KYIV_REVIEWED_STREET_ENTITIES } from './reviewed-streets.js';
 
 export const UA_KYIV_ENTITIES = Object.freeze([
   ...UA_KYIV_ANCHORS,
@@ -12,4 +13,5 @@ export const UA_KYIV_ENTITIES = Object.freeze([
   ...UA_KYIV_POI_ENTITIES,
   ...UA_KYIV_METRO_ENTITIES,
   ...UA_KYIV_STREET_ENTITIES,
+  ...UA_KYIV_REVIEWED_STREET_ENTITIES,
 ]);
