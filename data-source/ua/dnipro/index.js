c01a416bc7eeef2b1ef8bed2bacee0188761beac
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-05 21:00:54 +0300

    feat: wire reviewed Dnipro boulevards

--- data-source/ua/dnipro/index.js
+++ data-source/ua/dnipro/index.js
@@ -2,10 +2,12 @@ import { UA_DNIPRO_DISTRICT_ENTITIES } from './districts.js';
 import { UA_DNIPRO_NEIGHBORHOOD_ENTITIES } from './neighborhoods.js';
 import { UA_DNIPRO_RESIDENTIAL_COMPLEX_ENTITIES } from './residential-complexes.js';
 import { UA_DNIPRO_STREET_ENTITIES } from './streets.js';
+import { UA_DNIPRO_REVIEWED_BOULEVARD_ENTITIES } from './reviewed-boulevards.js';
 
 export const UA_DNIPRO_ENTITIES = Object.freeze([
   ...UA_DNIPRO_DISTRICT_ENTITIES,
   ...UA_DNIPRO_NEIGHBORHOOD_ENTITIES,
   ...UA_DNIPRO_RESIDENTIAL_COMPLEX_ENTITIES,
   ...UA_DNIPRO_STREET_ENTITIES,
+  ...UA_DNIPRO_REVIEWED_BOULEVARD_ENTITIES,
 ]);
