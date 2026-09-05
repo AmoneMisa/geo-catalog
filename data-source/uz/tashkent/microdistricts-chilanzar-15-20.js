53baec98e04d394a077ac37099235b341ba8d1fa
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-05 20:00:16 +0300

    fix: move Chilanzar-20 relation to canonical module

--- data-source/uz/tashkent/microdistricts-chilanzar-15-20.js
+++ data-source/uz/tashkent/microdistricts-chilanzar-15-20.js
@@ -1,4 +1,4 @@
-const osmMicrodistrict = (slug, canonicalName, parentId, lat, lng, osmId, accuracyM = 500) => ({
+const osmMicrodistrict = (slug, canonicalName, parentId, lat, lng, osmId, accuracyM = 500, osmType = 'node') => ({
   id: `uz:tashkent:microdistrict:${slug}`,
   type: 'microdistrict',
   country: 'UZ',
@@ -8,7 +8,7 @@ const osmMicrodistrict = (slug, canonicalName, parentId, lat, lng, osmId, accura
   source: 'osm',
   accuracy: 'neighborhood',
   accuracyM,
-  osm: { type: 'node', id: osmId },
+  osm: { type: osmType, id: osmId },
 });
 
 export const TASHKENT_CHILANZAR_15_20_ENTITIES = Object.freeze([
@@ -17,5 +17,5 @@ export const TASHKENT_CHILANZAR_15_20_ENTITIES = Object.freeze([
   osmMicrodistrict('chilanzar-17', 'Chilanzar-17', 'uz:tashkent:chilanzar', 41.27187, 69.19687, 1866861115),
   osmMicrodistrict('chilanzar-18', 'Chilanzar-18', 'uz:tashkent:chilanzar', 41.27326, 69.19131, 1865344355),
   osmMicrodistrict('chilanzar-19', 'Chilanzar-19', 'uz:tashkent:chilanzar', 41.2697, 69.18551, 1866856597),
-  osmMicrodistrict('chilanzar-20', 'Chilanzar-20', 'uz:tashkent:chilanzar', 41.26648, 69.18072, 1866856600),
+  osmMicrodistrict('chilanzar-20', 'Chilanzar-20', 'uz:tashkent:chilanzar', 41.2665051, 69.1798855, 1850375, 600, 'relation'),
 ]);
